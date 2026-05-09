91bbf2a3d3d0d50b1db89811e9529103c2d06a0f
{pierre-paul.quilichini} <{pierre-paul.quilichini@prehos.com}>
2026-05-09 19:42:20 -0400

    Add live Shopify locations debug page

--- app/root.tsx
+++ app/root.tsx
@@ -1,22 +1,28 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
+
+import { AppProvider as PolarisAppProvider } from "@shopify/polaris";
+import polarisTranslations from "@shopify/polaris/locales/en.json";
 import "@shopify/polaris/build/esm/styles.css";
 
 export default function App() {
   return (
-    <html lang="en">
+    <html>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link rel="preconnect" href="https://cdn.shopify.com/" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
-        />
         <Meta />
         <Links />
       </head>
+
       <body>
-        <Outlet />
+        <PolarisAppProvider i18n={polarisTranslations}>
+          <Outlet />
+        </PolarisAppProvider>
+
         <ScrollRestoration />
         <Scripts />
       </body>
